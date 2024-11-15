599f087f91000a4f2828b7cff72b51e3f25db4db
Fennrii <axsateagle@gmail.com>
2024-11-15 17:10:16 -0500

    Initial math setup (broken)

---
+++ mymath.h
@@ -0,0 +1,13 @@
+// mymath.h
+
+#ifdef MYMATH_H
+#define MYMATH_H
+
+
+double power(double num, double exponent);
+double natlog(double num);
+double logx(double num, double exponent);
+double gradient_est(double guess, int exponent);
+double nroot(double num, double exponent);
+
+#endif 
